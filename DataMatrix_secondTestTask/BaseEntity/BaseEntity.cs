9c26e88989d2b90ab2c78a18db7652066181f71c
Дастанбекулы Фараби Дастанбекович <80412019+hauntff@users.noreply.github.com>
2024-04-30 11:11:03 +0500

    Add project files.

---
+++ DataMatrix_secondTestTask/BaseEntity/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataMatrix_secondTestTask.BaseEntity
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
